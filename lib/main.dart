1ac81de204eb63b84a35141c2cd65db6a2afd8de
Yao Josué <jyao2000@yahoo.fr>
2020-05-19 05:32:31 +0200

    Padding, margin

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp()
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,16 +12,27 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20),
-            color: Colors.white,
-            child: Center(child: Text('Hello'),),
-          ),
+          child:Column(
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Center(
+                  child: Text('Container 1'),
+                ),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+            ],
+          ) ,
         ),
       ),
     );
   }
 }
+
