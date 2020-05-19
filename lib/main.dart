0e53d57d97e97309c3f442e4cf681e16da13ae34
Yao Josué <jyao2000@yahoo.fr>
2020-05-19 05:22:56 +0200

    Safe area

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
       ),
     );
   }
